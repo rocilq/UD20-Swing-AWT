7d25b6c4368a5bed2fa484304e67f8568daa2d34
rocilq <87014236+rocilq@users.noreply.github.com>
2021-08-21 01:51:48 +0200

    Update ejerciciosApp.java

--- src/ejerciciosApp.java
+++ src/ejerciciosApp.java
@@ -3,6 +3,8 @@ import javax.swing.JFrame;
 public class ejerciciosApp {
 
 	public static void main(String[] args) {
+		// He comentat els ejercicis, per que funcionin els descomento un a un
+		
 		//Ejercicio1 ejercicio1 = new Ejercicio1();
 		//Ejercicio2 ejercicio2 = new Ejercicio2();
 		//Ejercicio3 ejercicio3 = new Ejercicio3();
